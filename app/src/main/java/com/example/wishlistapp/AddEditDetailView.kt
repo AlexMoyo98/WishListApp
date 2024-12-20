df30f91a0f4ab407c42b1b08622d877c39ea4ba0
Alex Moyo <76572132+AlexMoyo98@users.noreply.github.com>
2024-12-20 17:43:06 +0200

    adding room database

--- app/src/main/java/com/example/wishlistapp/AddEditDetailView.kt
+++ app/src/main/java/com/example/wishlistapp/AddEditDetailView.kt
@@ -10,10 +10,15 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
 import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TextFieldDefaults
-import androidx.compose.material3.Scaffold
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -25,6 +30,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.wishlistapp.data.Wish
+import kotlinx.coroutines.launch
 
 @Composable
 fun AddEditDetailView(
@@ -32,6 +39,21 @@ fun AddEditDetailView(
     viewModel: WishViewModel,
     navController: NavController
 ) {
+
+    val snackMessage = remember {
+        mutableStateOf("")
+    }
+    val scope = rememberCoroutineScope()
+    val scaffoldState = rememberScaffoldState()
+    if (id != 0L){
+        val wish = viewModel.getAWishById(id).collectAsState(initial = Wish(0L,"",""))
+        viewModel.wishTitleState = wish.value.title
+        viewModel.wishDescriptionState = wish.value.description
+    } else {
+        viewModel.wishTitleState = ""
+        viewModel.wishDescriptionState = ""
+    }
+
     Scaffold(
         topBar = {
             AppBarView(
@@ -40,6 +62,7 @@ fun AddEditDetailView(
                 else stringResource(id = R.string.add_wish)
             ) { navController.navigateUp() }
         },
+        scaffoldState = scaffoldState
     ) {
         Column(
             modifier = Modifier
@@ -70,9 +93,29 @@ fun AddEditDetailView(
                 if (viewModel.wishTitleState.isNotEmpty() &&
                     viewModel.wishDescriptionState.isNotEmpty()
                 ) {
-                    //TODO UpdateWish
+                    if (id != 0L) {
+                        viewModel.updateWish(
+                            Wish(
+                                id = id,
+                                title = viewModel.wishTitleState.trim(),
+                                description = viewModel.wishDescriptionState.trim())
+                        )
+                        snackMessage.value = "Fields have been updated "
+                    } else {
+                        //TODO AddWish
+                        viewModel.addWish(
+                            Wish(
+                                title = viewModel.wishTitleState.trim(),
+                                description = viewModel.wishDescriptionState.trim())
+                        )
+                        snackMessage.value = "Wish has been created"
+                    }
                 } else {
-                    //TODO AddWish
+                    snackMessage.value = "Enter fields to create a wish "
+                }
+                scope.launch {
+                    scaffoldState.snackbarHostState.showSnackbar(snackMessage.value)
+                    navController.navigateUp()
                 }
             }) {
                 Text(
---
+++ app/src/main/java/com/example/wishlistapp/data/Wishrespository.kt
@@ -0,0 +1,24 @@
+package com.example.wishlistapp.data
+
+import kotlinx.coroutines.flow.Flow
+
+class WishRepository (private val wishDao: WishDao){
+
+    fun addWish(wish: Wish){
+        wishDao.addWish(wish)
+    }
+
+    fun getWishes(): Flow<List<Wish>> = wishDao.getAllWishes()
+
+    fun getAWishById(id:Long): Flow<Wish>{
+        return wishDao.getWishById(id)
+    }
+
+    suspend fun updateAWish(wish: Wish){
+        wishDao.updateWish(wish)
+    }
+
+    suspend fun deleteAWish(wish: Wish){
+        wishDao.deleteWish(wish)
+    }
+}
